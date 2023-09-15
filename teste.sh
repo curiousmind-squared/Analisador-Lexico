2927342b96ea98cd42aab60e137cae0e99556538
curious <kalaiscguimaraes@gmail.com>
2023-09-15 06:40:46 -0300

    Adicionando um arquivo para rodar testes

---
+++ teste.sh
@@ -0,0 +1,15 @@
+echo "$1" > src.txt
+
+echo "Source code: "
+cat src.txt
+
+compilation_errors=$(gcc first_try.c 2>&1) # Redireciona stdout e stderror para var 
+# Não colocar nada entre essa var e o if!
+
+if [ $? -eq 0 ]; then # checa status do último comando executado
+    echo "Compilation successful"
+    ./a.out
+else
+    echo "Compilation failed with the following errors:"
+    echo "$compilation_errors"
+fi
